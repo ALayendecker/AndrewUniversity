05243477c79dda69a26f59cb63da3404e189f2a4
Alayen <alcommits@gmail.com>
2019-12-18 16:59:50 -0600

    Add project files.

---
+++ AndrewUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using AndrewUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AndrewUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ AndrewUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AndrewUniversity.MvcApplication" Language="C#" %>
