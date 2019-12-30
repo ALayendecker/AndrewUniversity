69c39d3ca7648ee35df82fc6f97bfac592f488c4
Alayen <alcommits@gmail.com>
2019-12-30 16:56:57 -0600

    Testing Stored Procedures

--- AndrewUniversity/DAL/SchoolContext.cs
+++ AndrewUniversity/DAL/SchoolContext.cs
@@ -22,6 +22,7 @@ namespace AndrewUniversity.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
