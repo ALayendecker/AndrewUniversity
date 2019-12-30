c2d6e84cf129944452e1f85cd856e226fb38b168
Alayen <alcommits@gmail.com>
2019-12-30 12:53:19 -0600

    Instructor views and controller added explicit loading to instructor index

---
+++ AndrewUniversity/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using AndrewUniversity.Models;
+
+namespace AndrewUniversity.ViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
